4917126e6863e0e3f02254c5aa2a2638ba007781
jojostx <ikuskid7@gmail.com>
2022-09-16 01:42:30 -0700

    ALX 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,6 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * print_fizz - prints "Fizz"
- *
- * Return: void
- */
-void print_fizz(void)
-{
-	_putchar('F');
-	_putchar('i');
-	_putchar('z');
-	_putchar('z');
-}
-
-/**
- * print_buzz - prints "Buzz"
- *
- * Return: void
- */
-void print_buzz(void)
-{
-	_putchar('B');
-	_putchar('u');
-	_putchar('z');
-	_putchar('z');
-}
 /**
  * main - a program that prints the numbers from 1 to 100,
  * * followed by a new line. But for multiples of 3 print Fizz instead
@@ -42,26 +18,26 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			print_fizz();
-			print_buzz();
+			printf("Fizz");
+			printf("Buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			print_fizz();
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			print_buzz();
+			printf("Buzz");
 		}
 		else
 		{
 			if (n >= 10)
-				_putchar((n / 10) + 48);
-			_putchar((n % 10) + 48);
+				putchar((n / 10) + 48);
+			putchar((n % 10) + 48);
 		}
 
 		if (n != 100)
-			_putchar(' ');
+			putchar(' ');
 	}
 
 	return (0);
