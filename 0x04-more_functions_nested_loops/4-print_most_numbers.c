e960f382a50839adf83b9ec4db777c9e46e9360f
jojostx <ikuskid7@gmail.com>
2022-09-16 02:20:20 -0700

    ALX 0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,9 @@ void print_most_numbers(void)
 
 	while (n < 58)
 	{
-		if (n == 2 || n == 4)
-			continue;
+		if (n != 2 || n != 4)
+			_putchar(n);
 
-		_putchar(n);
 		n++;
 	}
 	_putchar('\n');
