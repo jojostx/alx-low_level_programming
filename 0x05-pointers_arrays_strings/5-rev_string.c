6f29bc195fe7527f14fd492f1303a743fb919440
jojostx <ikuskid7@gmail.com>
2022-09-19 16:57:14 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: char
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+
+	for (i = 0; i < count; i++)
+	{
+		char temp = s[count - 1];
+
+		s[count - 1] = s[i];
+		s[i] = temp;
+		count--;
+	}
+}
