575982c01a20bd998d57c1ca44dec261607bf898
jojostx <ikuskid7@gmail.com>
2022-09-19 17:09:49 -0700

    task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,15 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while (*(str + count) != '\0')
+	while (count >= 0)
 	{
-		_putchar(*(str + count));
-		count += 2;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
