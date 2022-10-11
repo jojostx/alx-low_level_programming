2ee6dcf5e378319134125a1a4e128567ca52d6eb
jojostx <ikuskid7@gmail.com>
2022-10-10 17:10:24 -0700

    correction for 1-init_dog,c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (dog){
-		dog->name = name;
-		dog->age = age;
-		dog->owner = owner;
+	if (d){
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
